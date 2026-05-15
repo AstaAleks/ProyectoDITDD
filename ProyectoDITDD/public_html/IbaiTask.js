694a99638467892cdb6cb2805ce1f7d9daaebf7a
PID <PID@DESKTOP-G0JACT0>
2026-05-15 09:46:03 +0200

    Hemos arreglado el index y hemos añadido el primer paso de la aplicacion.

--- ProyectoDITDD/public_html/IbaiTask.js
+++ ProyectoDITDD/public_html/IbaiTask.js
@@ -1,4 +1,16 @@
-"use_scrict"
+"use_scrict";
+
+let menu=prompt("¡Hola Buenas! Bienvenido a ibaiTask, la mejor aplicación para organizarse\n\n\
+ A continuacion, veras las opciones que tienes: \n\n\
+· Añadir Task(01)\n\
+· Ver la lista de Tasks(02)\n\
+· Modiuficar la Task(03)\n\
+· Eliminar Task(04)\n\
+· Salir de la aplicación(05)\n\
+\n\
+Tienes que elegir una de estas opciones,\n\
+ para elegir alguna, escribe el numero asignado de cada uno");
+
 
 
 
