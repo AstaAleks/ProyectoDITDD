19c0e79512516f467aedf2740eba08410e7026c8
PID <PID@DESKTOP-G0JACT0>
2026-05-18 12:49:21 +0200

    Arreglando y implementando nuevas cosas del inicio.

--- ProyectoDITDD/public_html/IbaiTask.js
+++ ProyectoDITDD/public_html/IbaiTask.js
@@ -1,6 +1,8 @@
 "use_scrict";
 
-let menu=prompt("¡Hola Buenas! Bienvenido a ibaiTask, la mejor aplicación para organizarse\n\n\
+
+alert("¡Hola Buenas! Bienvenido a ibaiTask, la mejor aplicación para organizarse.\n\
+En esta aplicación, podràs organizarte con las siguientes opciones.\n\n\
  A continuacion, veras las opciones que tienes: \n\n\
 · Añadir Task(01)\n\
 · Ver la lista de Tasks(02)\n\
@@ -10,6 +12,15 @@ let menu=prompt("¡Hola Buenas! Bienvenido a ibaiTask, la mejor aplicación para
 \n\
 Tienes que elegir una de estas opciones,\n\
  para elegir alguna, escribe el numero asignado de cada uno");
+let menu=prompt("Que opcion eliges? \n\n\
+· Añadir Task(01)\n\
+· Ver la lista de Tasks(02)\n\
+· Modiuficar la Task(03)\n\
+· Eliminar Task(04)\n\
+· Salir de la aplicación(05)\n\
+\n\
+Tienes que elegir una de estas opciones,\n\
+ para elegir alguna, escribe el numero asignado de cada uno.");
 
 
 
