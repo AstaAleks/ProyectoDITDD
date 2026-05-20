625fb36ae47391a4212d4748607d4cff83ad0745
PID <PID@DESKTOP-G0JACT0>
2026-05-20 11:55:01 +0200

    implementando las opciones con sus respuestas

--- ProyectoDITDD/public_html/IbaiTask.js
+++ ProyectoDITDD/public_html/IbaiTask.js
@@ -1,3 +1,5 @@
+/* global i */
+
 "use_scrict";
 
 
@@ -15,12 +17,32 @@ Tienes que elegir una de estas opciones,\n\
 let menu=prompt("Que opcion eliges? \n\n\
 · Añadir Task(01)\n\
 · Ver la lista de Tasks(02)\n\
-· Modiuficar la Task(03)\n\
+· Modificar la Task(03)\n\
 · Eliminar Task(04)\n\
 · Salir de la aplicación(05)\n\
 \n\
 Tienes que elegir una de estas opciones,\n\
  para elegir alguna, escribe el numero asignado de cada uno.");
+
+let accio;
+let titol;
+let hora;
+const llistat=[ ];
+let TascaModif;
+let missatge=" ";
+
+switch(menu){
+    case "01":
+            titol=prompt("Como quieres llamar esta tarea?");
+            llistat[i]=titol;
+    break;
+case "02":
+    "Aqui tienes la lista de tareas:"+llistat+".";
+    break;
+    
+            
+}
+        
 
 
 
