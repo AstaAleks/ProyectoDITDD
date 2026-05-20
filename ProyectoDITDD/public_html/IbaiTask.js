0d2f5e5c68eb2d54f53a4f5b75012a2c6aac606d
PePePePePedro <pedromarinacla@gmail.com>
2026-05-20 12:08:52 +0200

    Modificació IbaiTask.js
    Afegit el treball fet per en Pedro

--- ProyectoDITDD/public_html/IbaiTask.js
+++ ProyectoDITDD/public_html/IbaiTask.js
@@ -30,6 +30,15 @@ let hora;
 const llistat=[ ];
 let TascaModif;
 let missatge=" ";
+let accion;
+let afegirTitol;
+let afegirHora;
+let veureLlista;
+let modificarTasca;
+let modificarTitol;
+let modificarHora;
+let eliminarTasca;
+const tasques = [];
 
 switch(menu){
     case "01":
@@ -42,6 +51,29 @@ case "02":
     
             
 }
+
+"use_strict";
+
+
+
+accion = prompt("Que vols fer? Pots Afegir tasca, Veure llista de tasques, Modificar tasca, Eliminar Tasca i Sortir de l'aplicacio");
+
+while (accion = !"Sortir de l'aplicacio") {
+    accion = prompt("Que vols fer? Pots Afegir tasca, Veure llista de tasques, Modificar tasca, Eliminar Tasca i Sortir de l'aplicacio");
+}
+
+if (accion == "Afegir tasca") {
+        afegirTitol=prompt("Digues el titol de la tasca que vols afegir:");
+        afegirHora=prompt("Digues la data màxima de la tasca "+afegirTitol+":");
+    } else if (accion == "Veure llista de tasques") {
+        alert(veureLlista);
+    } else if (accion == "Modificar tasca") {
+        modificarTitol=prompt("Digues el titol de la tasca que vols ")
+    } else if (accion == "Eliminar tasca") {
+        alert(eliminarTasca);
+    } else if (accion == "Sortir de l'aplicacio") {
+        alert("Fins la proxima, màster!");
+    }
         
 
 
